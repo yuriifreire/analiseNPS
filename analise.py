9181072cfba5c083b1a3305ded8bf5f1e5f075ba
Yuri Freire <yuriifreire@gmail.com>
2024-12-17 15:22:32 -0300

    add calculo de NPS e CSAT por empresa
    adicionado funções com os cálculos de NPS e CSAT, dividido por empresa, conforme formula pré-existente

--- analise.py
+++ analise.py
@@ -18,7 +18,7 @@ def classificar_nps(nota):
 #Os Satisfeitos tem notas 4 e 5, os Neutros tem notas 3 e os Instatisfeitos tem notas 1 e 2
 def classificar_csat(nota):
     if nota >= 4:
-        return "Satisifeito"
+        return "Satisfeito"
     elif nota == 3:
         return "Neutro"
     else:
@@ -28,7 +28,7 @@ def classificar_csat(nota):
 df['Classificação NPS'] = df['NPS'].apply(classificar_nps)
 df['Classificação CSAT'] = df['CSAT'].apply(classificar_csat)
 
-#exibir planilha classificada
+#Exibir planilha classificada
 print(df)
 
 #Fazendo a separação de NPS por empresa
@@ -37,4 +37,30 @@ print(nps_por_empresa)
 
 #Fazendo a separação de CSAT por empresa
 csat_por_empresa = df.groupby('EMPRESA')['Classificação CSAT'].value_counts()
+print(csat_por_empresa)
+
+#Calcular o NPS por Empresa
+def calcular_nps(grupo):
+    total = len(grupo)
+    promotores = len(grupo[grupo['Classificação NPS'] == "Promotor"])
+    detratores = len(grupo[grupo['Classificação NPS'] == "Detrator"])
+    nps = ((promotores - detratores) / total) * 100 #multiplicamos por 100 por se tratar de um valor em porcentagem
+    return nps
+
+#Calcular o CSAT por Empresa
+def calcular_csat(grupo):
+    total = len(grupo)
+    satisfeitos = len(grupo[grupo['Classificação CSAT'] == "Satisfeito"])
+    csat = (satisfeitos / total) * 100
+    return csat
+
+
+#Passando a função do cálculo de NPS por empresa
+nota_nps_empresa = df.groupby('EMPRESA').apply(calcular_nps).reset_index()
+nota_nps_empresa.columns = ['EMPRESA', 'NPS']
+print(nota_nps_empresa)
+
+#Passando a função do cálculo de CSAT por empresa
+nota_csat_empresa = df.groupby('EMPRESA').apply(calcular_csat).reset_index()
+nota_csat_empresa.columns = ['EMPRESA','CSAT']
 print(csat_por_empresa)
